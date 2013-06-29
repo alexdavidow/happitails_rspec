77190b29bcbb3470e9776ca57baedb866d9889af
alexdavidow <alex.davidow@gmail.com>
2013-06-29 16:09:26 -0400

    add and remove animal within client working

--- client.rb
+++ client.rb
@@ -1,7 +1,9 @@
 class Client
-  def initialize(c_name, c_age)
+  def initialize(c_name, c_age, c_gender, animal_list)
     @name = c_name
     @age = c_age
+    @gender = c_gender
+    @animal_list = animal_list
   end
 
   def name
@@ -10,5 +12,21 @@ class Client
 
   def age
     @age
+  end
+
+  def gender
+    @gender
+  end
+
+  def animal_list
+    @animal_list
+  end
+
+  def remove_animal(index)
+    @animal_list.delete_at(index)
+  end
+
+  def add_animal(animal)
+    @animal_list << animal
   end
 end
--- client_spec.rb
+++ client_spec.rb
@@ -6,8 +6,9 @@ class ClientSpec
     before do
       @c_name = 'alex'
       @c_age = 26
-      @c_gender
-      @client = Client.new(@c_name, @c_age, @c_gender)
+      @c_gender = 'male'
+      @animal_list = ['bonzo', 'kensington']
+      @client = Client.new(@c_name, @c_age, @c_gender, @animal_list)
     end 
 
     it 'has a name' do
@@ -18,7 +19,23 @@ class ClientSpec
       @client.age.should eq @c_age
     end
 
-    it 'has a gender'
-    it 'has a list of animals'  
+    it 'has a gender' do
+      @client.gender.should eq @c_gender
+    end
+    it 'has a list of animals' do
+      @client.animal_list.should eq @animal_list
+    end  
+    
+    it 'can remove an animal' do
+      numAnimals = @animal_list.length
+      @client.remove_animal(0)
+      @client.animal_list.length.should eq (numAnimals - 1)
+    end
+
+    it 'can add an animal' do
+      numAnimals = @animal_list.length
+      @client.add_animal(0)
+      @client.animal_list.length.should eq (numAnimals + 1)
+    end
   end
 end
